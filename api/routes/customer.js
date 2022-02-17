df54dc420831a97fc863863e1610f2d68c83ee77
Aryane Ladeira <69313233+AryaneLadeira@users.noreply.github.com>
2022-02-17 14:42:35 -0300

    Desaclopando API

--- api/routes/customer.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-    const controller = require('../controllers/customer')();
-  
-    app.route('/api/customer')
-      .get(controller.listCustomerWallets);
-  }
--- js/index.js
+++ js/index.js
@@ -1,28 +1,10 @@
-(async () => {
-    const db = require("./db");
-    console.log("Começou!");
 
-    // console.log('Insert into clientes');
-    // const result = await db.insertCustomer({nome: "Zé"});
-    // console.log(result);
+async function teste() {
+    fetch('http://localhost:8080/api/customer').then((res)=>{
+        return res.json()
+    }).then((body)=> {
+        console.log(body);
+    })
+}
 
-    // console.log('update clientes');
-    // const result2 = await db.updateCustomer(1, {nome: "Zé2"});
-    // console.log(result2);
-
-    console.log('delete cliente');
-    const result3 = await db.deleteCustomer(1);
-    console.log(result3);
-
-    console.log('Select * from clientes');
-    const clientes = await db.selectCustomers();
-    console.log(clientes);
-})();
-
-// async function teste() {
-//     fetch('./db').then((res)=>{
-//         console.log(res);
-//     })
-// }
-
-// teste();
+teste();
